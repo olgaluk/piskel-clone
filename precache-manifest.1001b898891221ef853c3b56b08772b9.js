6a9c03baf423f22883258303bc684ac259431b43
Olga Luk <olga_ivanova_86@mail.ru>
2019-07-08 01:02:13 +0300

    feat: add project build

---
+++ precache-manifest.1001b898891221ef853c3b56b08772b9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30807ed58bb05923cf4bc7e458d7653d",
+    "url": "/piskel-clone/index.html"
+  },
+  {
+    "revision": "2f91364f5f1bc2fe2631",
+    "url": "/piskel-clone/static/css/main.4d7a6460.chunk.css"
+  },
+  {
+    "revision": "20ac97eaf656499b4fc6",
+    "url": "/piskel-clone/static/js/2.63d0e195.chunk.js"
+  },
+  {
+    "revision": "2f91364f5f1bc2fe2631",
+    "url": "/piskel-clone/static/js/main.36ba762c.chunk.js"
+  },
+  {
+    "revision": "5654ab04200be8a1ca6b",
+    "url": "/piskel-clone/static/js/runtime~main.dff1cf59.js"
+  },
+  {
+    "revision": "b2e81a6505bce9f548a08cbcc2d91b30",
+    "url": "/piskel-clone/static/media/Screenshots1.b2e81a65.png"
+  },
+  {
+    "revision": "6e9d06780b7cad7bec9c1920ecae9dcb",
+    "url": "/piskel-clone/static/media/Screenshots2.6e9d0678.png"
+  },
+  {
+    "revision": "39b3486a0859724b627e501ea7d44ef7",
+    "url": "/piskel-clone/static/media/Screenshots3.39b3486a.png"
+  },
+  {
+    "revision": "5e89428246c67e880c1f8c47531967f7",
+    "url": "/piskel-clone/static/media/back1.5e894282.png"
+  },
+  {
+    "revision": "a2ba5f0797ca910934249d9269b650ab",
+    "url": "/piskel-clone/static/media/fullscreen.a2ba5f07.png"
+  }
+]);
